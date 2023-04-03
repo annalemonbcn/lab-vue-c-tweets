2be57bc27a9c9d6985e78c3cce74a19bc5197620
Anna Esteve <aesteve8@gmail.com>
2023-04-03 18:59:22 +0200

    all done

---
+++ src/components/ProfileImage.vue
@@ -0,0 +1,19 @@
+<template>
+  <img :src="imageUrl" class="profile" alt="profile" />
+</template>
+
+<script>
+export default {
+  name: 'ProfileImage',
+  props: {
+    imageUrl: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Timestamp.vue
@@ -0,0 +1,19 @@
+<template>
+  <span class="timestamp">{{ time }}</span>
+</template>
+
+<script>
+export default {
+  name: 'Timestamp',
+  props: {
+    time: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
